9ec1fe3b108bfca545eb0042f140d7d694aa5eaa
zhangvj <zhangvj1995@gmail.com>
2025-06-27 15:31:23 +0800

    delete: 删除多余的视图和组件文件。

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,22 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    // 是否开启 https
+    https: false,
+    // 端口号
+    port: 3000,
+    // 监听所有地址
+    host: '0.0.0.0',
+    // 服务启动时是否自动打开浏览器
+    open: false,
+    // 允许跨域
+    cors: true,
   },
 })
